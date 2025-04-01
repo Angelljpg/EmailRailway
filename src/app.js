bc3ff314a782cc34d47d452a1e1e0ad28fb51975
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2025-03-31 23:48:25 -0600

    add cors configuration

--- src/app.js
+++ src/app.js
@@ -2,15 +2,9 @@ import bodyParser from "body-parser";
 import express from "express";
 import emailRoutes from "./routes/emailRoutes.js";
 import { userEvents, passwordResetEvents } from "./services/rabbitServiceListener.js";
-import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
-    origin: "*",  // Puedes cambiar esto a la URL del ESB si deseas restringir el acceso
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 app.use(bodyParser.json());
 app.use('/api/email', emailRoutes);
